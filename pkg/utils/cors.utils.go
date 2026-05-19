38c46775588bbccaf0509e7bf3708de770644ceb
aqilknz <khairunnz123@gmail.com>
2026-05-19 10:16:17 +0700

    feat(utils): add manual CORS handling and preflight configuration

---
+++ pkg/utils/cors.utils.go
@@ -0,0 +1,33 @@
+package utils
+
+import (
+	"net/http"
+	"slices"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+// CORSMiddleware adalah fungsi penjaga untuk mengatur siapa saja yang boleh mengakses API
+// "http://127.0.0.1:5500"
+func CORSMiddleware(ctx *gin.Context) {
+	allowedOrigin := []string{"http://localhost:5173", "http://localhost:3000"}
+
+	currentOrigin := ctx.GetHeader("Origin")
+	if slices.Contains(allowedOrigin, currentOrigin) {
+		ctx.Header("Access-Control-Allow-Origin", currentOrigin)
+	}
+
+	allowedHeaders := []string{"Content-Type", "Authorization"}
+	ctx.Header("Access-Control-Allow-Headers", strings.Join(allowedHeaders, ", "))
+
+	allowedMethods := []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete, http.MethodPatch, http.MethodOptions}
+	ctx.Header("Access-Control-Allow-Methods", strings.Join(allowedMethods, ", "))
+
+	if ctx.Request.Method == http.MethodOptions {
+		ctx.AbortWithStatus(http.StatusNoContent)
+		return
+	}
+
+	ctx.Next()
+}
