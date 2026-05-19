5c511d1813670e3fd390a0b732c53de67d740833
aqilknz <khairunnz123@gmail.com>
2026-05-19 11:21:14 +0700

    feat(main): setup application entry point and server initialization

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
 
+	"github.com/aqilknz/backend-ewallet/internal/config"
+	"github.com/aqilknz/backend-ewallet/internal/router"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading env. \ncause: %s", err.Error())
+	}
+
+	// inisialisasi
+	app := gin.Default()
+
+	// connect ke db
+	db, err := config.ConnectDB(context.Background())
+	if err != nil {
+		log.Fatalf("DB connection error. \ncause: %s", err.Error())
+	}
+	defer db.Close()
+	log.Println("DB Connected")
+
+	// install router
+	router.InitRouter(app, db)
+
+	// run
+	app.Run(fmt.Sprintf("%s:%s", os.Getenv("APP_HOST"), os.Getenv("APP_PORT")))
 }
