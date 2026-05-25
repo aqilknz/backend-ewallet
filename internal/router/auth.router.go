b1c449f2d0e60c1e811ca82cafb6f57a94acec63
aqilknz <khairunnz123@gmail.com>
2026-05-25 07:45:32 +0700

    feat: add route for logout

--- internal/router/auth.router.go
+++ internal/router/auth.router.go
@@ -2,12 +2,15 @@ package router
 
 import (
 	"github.com/aqilknz/backend-ewallet/internal/controller"
+	"github.com/aqilknz/backend-ewallet/internal/middleware"
+	"github.com/aqilknz/backend-ewallet/internal/repository"
 	"github.com/gin-gonic/gin"
 )
 
 // RegisterAuthRoutes
-func RegisterAuthRoutes(rg *gin.RouterGroup, authController *controller.AuthController) {
+func RegisterAuthRoutes(rg *gin.RouterGroup, authController *controller.AuthController, authRepo repository.AuthRepository) {
 	authGroup := rg.Group("/auth")
 	authGroup.POST("/register", authController.Register)
 	authGroup.POST("/", authController.Login)
+	authGroup.DELETE("/logout", middleware.RequireAuth(authRepo), authController.Logout)
 }
