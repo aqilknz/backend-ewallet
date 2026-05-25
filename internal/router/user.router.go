1f2d11a375f9e21cf6a53cac28d2c6d92c029b4e
aqilknz <khairunnz123@gmail.com>
2026-05-25 07:47:43 +0700

    feat: add auth repo for param

--- internal/router/user.router.go
+++ internal/router/user.router.go
@@ -3,22 +3,24 @@ package router
 import (
 	"github.com/aqilknz/backend-ewallet/internal/controller"
 	"github.com/aqilknz/backend-ewallet/internal/middleware"
+	"github.com/aqilknz/backend-ewallet/internal/repository"
 	"github.com/gin-gonic/gin"
 )
 
 // RegisterUserRoutes mendaftarkan rute yang butuh proteksi token
-func RegisterUserRoutes(rg *gin.RouterGroup, userController *controller.UserController) {
+func RegisterUserRoutes(rg *gin.RouterGroup, userController *controller.UserController, authRepo repository.AuthRepository) {
 	userGroup := rg.Group("/users")
 
 	//Semua rute di bawah userGroup otomatis harus bawa Token JWT
-	userGroup.Use(middleware.RequireAuth())
+	userGroup.Use(middleware.RequireAuth(authRepo))
 	// Ambil Data
 	userGroup.GET("/profile", userController.GetProfile)
 	userGroup.GET("/dashboard", userController.GetDashboard)
 
 	// Update Data
-	userGroup.PUT("/profile", userController.EditProfile)
+	userGroup.PATCH("/profile", userController.EditProfile)
 	userGroup.PATCH("/profile/password", userController.EditPassword)
 	userGroup.PATCH("/profile/pin", userController.EditPin)
 	userGroup.POST("/transaction/checkpin", userController.CheckPin)
+	userGroup.GET("/receivers", userController.FindReceivers)
 }
