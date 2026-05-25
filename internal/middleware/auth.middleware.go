582001af31b071fdf479a674f272df28362d2086
aqilknz <khairunnz123@gmail.com>
2026-05-25 07:41:13 +0700

    feat: add custom response and add param auth

--- internal/middleware/auth.middleware.go
+++ internal/middleware/auth.middleware.go
@@ -1,40 +1,43 @@
 package middleware
 
 import (
-	"net/http"
 	"strings"
 
+	"github.com/aqilknz/backend-ewallet/internal/repository"
+	"github.com/aqilknz/backend-ewallet/internal/response"
 	"github.com/aqilknz/backend-ewallet/pkg"
 	"github.com/gin-gonic/gin"
 )
 
 // RequireAuth adalah middleware untuk mengecek JWT Token
-func RequireAuth() gin.HandlerFunc {
+func RequireAuth(authRepo repository.AuthRepository) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// Ambil Header Authorization
 		authHeader := c.GetHeader("Authorization")
 		if authHeader == "" {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
-				"error": "Akses ditolak, silakan login terlebih dahulu",
-			})
+			response.JSONUnauthorized(c, "Akses ditolak", "Silakan login terlebih dahulu")
+			c.Abort()
 			return
 		}
 
 		parts := strings.Split(authHeader, " ")
 		if len(parts) != 2 || parts[0] != "Bearer" {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
-				"error": "Format token tidak valid (Gunakan: Bearer <token>)",
-			})
+			response.JSONUnauthorized(c, "Format token tidak valid", "Gunakan format: Bearer <token>")
+			c.Abort()
 			return
 		}
 
 		tokenString := parts[1]
+		if authRepo.IsTokenBlacklisted(c.Request.Context(), tokenString) {
+			response.JSONUnauthorized(c, "Sesi telah berakhir", "Silakan login kembali")
+			c.Abort()
+			return
+		}
 
 		userID, err := pkg.VerifyToken(tokenString)
 		if err != nil {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
-				"error": "Token tidak valid atau sudah kadaluarsa",
-			})
+			response.JSONUnauthorized(c, "Sesi tidak valid", "Token tidak valid atau sudah kadaluarsa")
+			c.Abort()
 			return
 		}
 
