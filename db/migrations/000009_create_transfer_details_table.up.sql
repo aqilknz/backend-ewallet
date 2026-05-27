8525e9440dc97dcc46da57752b5b239bae523ca8
aqilknz <khairunnz123@gmail.com>
2026-05-27 21:25:20 +0700

    feat: add update column transfer

--- db/migrations/000009_create_transfer_details_table.up.sql
+++ db/migrations/000009_create_transfer_details_table.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS transfer_details (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     transaction_id INT UNIQUE NOT NULL,
-    counterparty_id INT NOT NULL,
+    receiver_id INT NOT NULL,
     notes TEXT,
     FOREIGN KEY (transaction_id) REFERENCES transactions(id) ON DELETE CASCADE,
-    FOREIGN KEY (counterparty_id) REFERENCES users(id)
+    FOREIGN KEY (receiver_id) REFERENCES users(id)
 );
